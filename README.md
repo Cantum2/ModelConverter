3acd68803c8354f80ffdef0536b907db5dd58e8d
Marcus Cantu <cantumarcus19@gmail.com>
2020-03-20 13:52:02 -0600

    fixed the contructor string creator

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Model Converter
+A quick script to convert TS models to Dart models. 
+
+## What is this? 
+This is a step towards developing a script that can scan through your typescript project files, find the models and turn them into syntactically correct dart models. 
+
+### Why would I need this? 
+This is super useful when working with a project that uses both Angular and Flutter. Your models may be developed already in Typescript and you may need them in your Flutter version of your application as well. Using this script will eliminate possible spelling errors or human made errors when translating your models. 
+
+### Why does the code look like it was wrote by a fifth grader?
+This was super quickly coded up and has plenty of room for improvements!
+
+### Running 
+1. Open the folder in your python editor 
+2. Create a folder called `dump` in the root of the project
+3. Run the script and enter the directory of your typescript project 
+4. The script will scan the directory and convert ts syntax to dart, rename the file to have `.dart` and save them to the dump directory
+
+### Contributing
+Currently it works only one way (TS to Dart) it would be awesome to go the other way as well. 
+Feel free to submit a PR of your code refactoring or feature additions! 
--- driver.py
+++ driver.py
@@ -35,35 +35,65 @@ def type_check_bool(rhs):
     return 'true' in rhs or 'false' in rhs
 
 def type_check(item_to_check):
-        if type_check_string_or_int(item_to_check) and type_check_list(item_to_check): 
-            # is a list of strings
-            return LIST+'<'+STRING+'>'+' '
-        
-        if type_check_string_or_int(item_to_check):
-            # is just string
-            return STRING
-        
-        if type_check_bool(item_to_check) and type_check_list(item_to_check):
-            # is list of booleans
-            return LIST+'<'+BOOL+">"+" "
-        
-        if type_check_bool(item_to_check):
-            # is just boolean 
-            return BOOL+ " "
+    """takes in the rhs of the line if declared and performs a series of checks and returns the possible dart type of the string
+    
+    Arguments:
+        item_to_check {string} -- rhs of the line 
+    
+    Returns:
+        string -- the completed type 
 
-        if not type_check_string_or_int(item_to_check) and type_check_list(item_to_check):
-            # is list of ints
-            return LIST + "<" + INT + ">"
+    Issues: 
+        If you have an array of objects it will declare it as List<int> instead of List<dymanic> 
+        TODO Check each element in the array and determine type from there
+    
+    Example: 
+        item_to_check = '25'
+            returns String
+        item_to_check = 25
+            return int
+        item_to_check = ['Test']
+            return List<String>
+    """
+    if type_check_string_or_int(item_to_check) and type_check_list(item_to_check): 
+        # is a list of strings
+        return LIST+'<'+STRING+'>'+' '
+    
+    if type_check_string_or_int(item_to_check):
+        # is just string
+        return STRING
+    
+    if type_check_bool(item_to_check) and type_check_list(item_to_check):
+        # is list of booleans
+        return LIST+'<'+BOOL+">"+" "
+    
+    if type_check_bool(item_to_check):
+        # is just boolean 
+        return BOOL+ " "
 
-        if not type_check_string_or_int(item_to_check):
-            # is just int
-            return INT+ " "
+    if not type_check_string_or_int(item_to_check) and type_check_list(item_to_check):
+        # is list of ints
+        return LIST + "<" + INT + ">"
 
-def handle_ts_defined_line():
-    print('hi')
-
+    if not type_check_string_or_int(item_to_check):
+        # is just int
+        return INT+ " "
 
 def buildContructor(params, className):
+    """builds the dart contructor
+    
+    Arguments:
+        params {list of strings} -- variable names of the current class
+        className {string} -- current class name
+    
+    Returns:
+        string -- contructor string in dart syntax 
+
+    Example:
+        params = ['varone', 'vartwo', 'varthree']
+        classname = Example
+        returns Example({this.varone, this.vartwo, this.varthree,}) 
+    """
     cname = className.replace('class', '').replace('{', '').strip() #TODO pass in already formatted
     contructor_string = cname + '({'
     for i in params:
@@ -73,17 +103,24 @@ def buildContructor(params, className):
     return contructor_string
 
 def get_var_name(possible_name):
-    " ".join(possible_name.split())
-    most_recent_space = 0
-    most_most_recent_space = 0
-    for i in range(0, len(possible_name)):
-        if possible_name[i] == ' ':
-            most_recent_space = most_most_recent_space
-            most_most_recent_space = i
-        if possible_name[i] == '=':
-            print(possible_name[most_recent_space:most_most_recent_space])
-            print(most_most_recent_space, most_recent_space)
-    return possible_name[most_recent_space:most_most_recent_space]
+    """the possible name at this point will be some thing like  public VARNAME = 25;  
+    split the at the equal sign ['public VARNAME', '25']
+    now split at the spaces for the first element because we know the non empty token before the equal sign is the var name 
+    ['public', 'VARNAME']
+    return the last element in the above array 
+    
+    Arguments:
+        possible_name {string} -- the entire current line
+    
+    Returns:
+        string -- list name
+    """
+    sections = possible_name.split('=') 
+    split_spaces = sections[0].split(" ") 
+    split_spaces = [x for x  in split_spaces if x != ''] # 
+    print(split_spaces[-1])
+    return split_spaces[-1]
+
 
 def ts_to_dart(dir_name, file_name):
     formatted_path = dir_name+'\\'+file_name
@@ -93,40 +130,54 @@ def ts_to_dart(dir_name, file_name):
     try:
         file = open(formatted_path, 'r')
         dump_file = open(dump_path, 'w')
-        params = []
+        params = [] # list of params declared int he current class. Used in the contructor
         current_class_name = ''
         for line in file:
             dart_string = ''
             current_var_name = ''
+            #skip comments and imports
             if 'import' in line or '//' in line: 
                 continue
-            if '}' in line and len(line) < 5: # close class
-                dart_string += buildContructor(params, current_class_name)+'\n'
-                dart_string += line
-            if 'class' in line and "{" in line: # allows people to have variables named classes
+            # starting the current class
+            if 'class' in line and "{" in line:  # the { check allows people to have variables inside a class that have the name
                  # convert to dart class dec syntax
                 dart_string += 'class ' + line.split('class')[1] + '\n'
                 current_class_name = dart_string.replace('{', '') 
+            # this is the last line in the class
+            if line.count('}') == 1 and len(line) < 5: 
+                # build the contructor
+                dart_string += buildContructor(params, current_class_name)+'\n'
+                #add the } 
+                dart_string += line
             else:
-                items = line.split(':')
-                #no type was declared so rearragne keywords
+                items = []
+                # check if current line is a typescript object
+                if line.count(':') > 1:
+                    # if so treat 
+                    items = line.split('=')
+                else:
+                    # this means a type was declared varname: string;
+                    items = line.split(':')
+
+                # swap template literal syntax
+                if '`' in line: 
+                    line = line.replace('`', '\'')           
+
                 if len(items) == 1:
                     # check for initialalized var
                     decs = line.split('=')
                     if len(decs) > 1:
-                        if ask_for_const_all_files == '0':
-                            init_to_const = input('This line:\n' + '\t'+line + '\n in the file : '+'\''+file_name+'\''' has an inital value. \n Would you like it to be declared with const in Dart?\n Enter 0 for Yes to this instance. \n Enter 1 for Yes to all instances. \n Enter 2 for Yes to all non empty declerations\n Enter 3 for NOT on this instance. \n Enter 4 for NOT any instance.')
-                            if init_to_const == '1' or init_to_const == '2' or init_to_const == '4':
-                                ask_for_const_all_files = input('Would you like to be asked in every file? Enter 0 for Yes and 1 for No')
+                        #TODO ask if we they want const on every already declared value
+                        # if ask_for_const_all_files == '0':
+                        #     init_to_const = input('This line:\n' + '\t'+line + '\n in the file : '+'\''+file_name+'\''' has an inital value. \n Would you like it to be declared with const in Dart?\n Enter 0 for Yes to this instance. \n Enter 1 for Yes to all instances. \n Enter 2 for Yes to all non empty declerations\n Enter 3 for NOT on this instance. \n Enter 4 for NOT any instance.')
+                        #     if init_to_const == '1' or init_to_const == '2' or init_to_const == '4':
+                        #         ask_for_const_all_files = input('Would you like to be asked in every file? Enter 0 for Yes and 1 for No')
                         # variable was declared with initial value and no type
+
                         # build dec string with no type
                         if STATIC in decs[0] :
                             decs[0] = decs[0].replace('static', '')
-                            # check for static and 
-                            if init_to_const == '0' or init_to_const == '1' or (init_to_const == '2' and len(decs[1]) > 1):
-                                dart_string += STATIC+" "+CONST+" "
-                            else:
-                                dart_string += STATIC+" "
+                            dart_string += STATIC+" "+CONST+" "
 
                         dart_string += type_check(decs[1])
     
@@ -140,11 +191,6 @@ def ts_to_dart(dir_name, file_name):
                        
                         current_var_name =  get_var_name(line)
                         dart_string += " = "+decs[1]
-                        print(dart_string)
-
-                    #print(decs)
-                    #dart_string += 
-                #print(items)
                 else:
                     # type was declared
                     ts_type = items[1].replace(';', '').replace('\n','').strip()
@@ -157,7 +203,6 @@ def ts_to_dart(dir_name, file_name):
                             dart_type =  TS_TO_DART_TYPES[ts_type]
                         except KeyError:
                             dart_type = 'dynamic'
-                    print(dart_type)
                     dart_string += dart_type + items[0]+';\n'
                     current_var_name = items[0]
             if len(current_var_name) > 0:
@@ -166,7 +211,6 @@ def ts_to_dart(dir_name, file_name):
         file.close()
     except IOError:
         print('Error opening file')
-    #print(formatted_path)
 
 def dart_to_ts(dir_name, file_name):
     print(dir_name,file_name)
@@ -192,8 +236,7 @@ def crawl_folder(directory, conversion):
 
 
 def init():
-    dir = 'C:\Programming\Fitter2'
-    #input('Input File path')
+    dir = input('Input File path')
     conversion = '0' #input('Enter 0 for TS to Dart and 1 for Dart to TS')
     crawl_folder(dir, conversion)
 
